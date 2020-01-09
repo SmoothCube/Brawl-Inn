0b4bc490b57488509d8f483b9e0f4ddcfdb51a20
Espen <espenhg@hotmail.com>
2020-01-09 13:15:55 +0100

    Basic river functionality
    - can only push players, and not their meshes when they fall

--- Source/BrawlInn/Components/PunchComponent_B.cpp
+++ Source/BrawlInn/Components/PunchComponent_B.cpp
@@ -127,6 +127,7 @@ void UPunchComponent_B::GetPunched(FVector InPunchStrength)
 
 void UPunchComponent_B::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	
 	APlayerCharacter_B* OtherPlayer = Cast<APlayerCharacter_B>(OtherActor);
 	UCapsuleComponent* Capsule = Cast<UCapsuleComponent>(OtherComp);
 
