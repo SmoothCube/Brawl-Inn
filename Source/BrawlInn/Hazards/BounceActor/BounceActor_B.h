8e6e019e6e0a22b7503ef827d6df204cbf80bc74
Espen <espenhg@hotmail.com>
2020-01-13 11:55:41 +0100

    various small stuff
    - barrels have a texture

--- Source/BrawlInn/Hazards/BounceActor/BounceActor_B.h
+++ Source/BrawlInn/Hazards/BounceActor/BounceActor_B.h
@@ -18,6 +18,10 @@ public:
 	// Sets default values for this actor's properties
 	ABounceActor_B();
 
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
