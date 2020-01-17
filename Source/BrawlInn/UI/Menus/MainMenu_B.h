4a1a1be2dc1cbb8c22cf16e5ddb4b407935b3c17
Jonas Andersen <20195223+NotBjoggisAtAll@users.noreply.github.com>
2020-01-17 08:57:42 +0100

    Added MenuMap and a MainMenu class

---
+++ Source/BrawlInn/UI/Menus/MainMenu_B.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu_B.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRAWLINN_API UMainMenu_B : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
