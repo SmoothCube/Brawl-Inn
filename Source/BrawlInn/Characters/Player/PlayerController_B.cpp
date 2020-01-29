7e4727d9e8282c7134188e6dafc7c5a52f065440
Jonas Andersen <jonas.bjorge.andersen@gmail.com>
2020-01-29 21:44:40 +0100

    Update PlayerController_B.cpp

--- Source/BrawlInn/Characters/Player/PlayerController_B.cpp
+++ Source/BrawlInn/Characters/Player/PlayerController_B.cpp
@@ -1,10 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "PlayerController_B.h"
+
 #include "Engine/World.h"
 #include "Kismet/GameplayStatics.h"
 #include "BrawlInn.h"
 
-#include "PlayerController_B.h"
 #include "Characters/Player/PlayerCharacter_B.h"
 #include "Characters/Player/RespawnPawn_B.h"
 #include "Components/HoldComponent_B.h"
