dc45272619b06570f580614eb7bbfdc72da3cc63
Espen <espenhg@hotmail.com>
2020-02-20 12:27:03 +0100

    manually adds players that break free back into camera focus

--- Source/BrawlInn/System/Camera/GameCamera_B.cpp
+++ Source/BrawlInn/System/Camera/GameCamera_B.cpp
@@ -183,7 +183,7 @@ void AGameCamera_B::OnTrackingBoxEndOverlap(UPrimitiveComponent* OverlappedCompo
 
 void AGameCamera_B::OnTrackingBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-
+	BWarn("Begin overlap with: %s, Owner: %s", *GetNameSafe(OtherActor), *GetNameSafe(OtherActor->GetOwner()))
 	//if (OtherActor->IsA(APlayerCharacter_B::StaticClass()))	
 	//	ActorsToTrack.Add(OtherActor);
 }
