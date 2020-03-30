181f9a38527d25c5dfbce3236270d3faf19535ed
Jonas Andersen <jonas.bjorge.andersen@gmail.com>
2020-03-30 13:32:43 +0200

    Removed G

--- Source/BrawlInn/UI/Widgets/ScoreText_B.cpp
+++ Source/BrawlInn/UI/Widgets/ScoreText_B.cpp
@@ -5,7 +5,7 @@
 
 void UScoreText_B::DisplayScore(const float Score)
 {
-	ScoreText->SetText(FText::FromString("+ " + FString::FormatAsNumber(Score) + " G"));
+	ScoreText->SetText(FText::FromString("+" + FString::FormatAsNumber(Score)));
 	ScoreText->SetVisibility(ESlateVisibility::Visible);
 }
 
