2a68ec14463180a86c0ae464459460117b466fed
Jonas Andersen <jonas.bjorge.andersen@gmail.com>
2020-04-03 10:33:20 +0200

    The characters in the vicotory map is changing to the correct material depending on who won
    The rest of the characters is deleted.

--- Source/BrawlInn/System/GameModes/VictoryGameMode_B.cpp
+++ Source/BrawlInn/System/GameModes/VictoryGameMode_B.cpp
@@ -7,6 +7,8 @@
 #include "Kismet/GameplayStatics.h"
 #include "Engine/World.h"
 #include "Camera/CameraActor.h"
+#include "Characters/Player/PlayerCharacter_B.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "Engine/LocalPlayer.h"
 
 #include "System/GameInstance_B.h"
@@ -26,6 +28,36 @@ void AVictoryGameMode_B::BeginPlay()
 		APlayerController* Controller = UGameplayStatics::GetPlayerControllerFromID(GetWorld(), Info.ID);
 		BWarn("Player Controller ID: %i with %i as score.", Info.ID, Controller->GetLocalPlayer()->GetSubsystem<UScoreSubSystem_B>()->GetScoreValues().Score);
 	}
+
+	TArray<AActor*> OutActors;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), "Players", OutActors);
+
+	TArray<FName> Tags;
+	Tags.Add("First");
+	Tags.Add("Second");
+	Tags.Add("Third");
+	Tags.Add("Fourth");
+
+	//Finds the characters to keep and updates their Mesh Material.
+	TArray<AActor*> CharactersToKeep;
+	for (int i = 0; i < GameInstance->GetPlayerInfos().Num(); i++)
+	{
+		const auto Character = OutActors.FindByPredicate([&](AActor* Actor)
+			{
+				return Actor->ActorHasTag(Tags[i]);
+			});
+		CharactersToKeep.Add(*Character);
+		APlayerCharacter_B* PlayerCharacter = Cast<APlayerCharacter_B>(*Character);
+		PlayerCharacter->GetMesh()->CreateAndSetMaterialInstanceDynamicFromMaterial(1, GameInstance->GetPlayerInfos()[i].CharacterVariant.MeshMaterial);
+		PlayerCharacter->GetDirectionIndicatorPlane()->DestroyComponent();
+	}
+
+	//Removes and destroys the characters that isn't supposed to be there
+	for (auto Actor : CharactersToKeep)
+		OutActors.Remove(Actor);
+
+	for (auto Actor : OutActors)
+		Actor->Destroy();
 }
 
 void AVictoryGameMode_B::PostLevelLoad()
