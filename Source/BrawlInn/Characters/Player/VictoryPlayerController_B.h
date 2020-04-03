81a3785a8a21e152ee59283e619b905c948ed128
Jonas Andersen <jonas.bjorge.andersen@gmail.com>
2020-04-03 10:33:34 +0200

    Added a VictoryPlayerController

---
+++ Source/BrawlInn/Characters/Player/VictoryPlayerController_B.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/Player/PlayerController_B.h"
+#include "VictoryPlayerController_B.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRAWLINN_API AVictoryPlayerController_B : public APlayerController_B
+{
+	GENERATED_BODY()
+	
+};
