a4edeadacc283c4160ddb6790fdd11bb8e56e3e5
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2020-04-15 11:38:13 +0200

    Update MenuMapLevelScriptActor_B.cpp

--- Source/BrawlInn/System/Maps/MenuMapLevelScriptActor_B.cpp
+++ Source/BrawlInn/System/Maps/MenuMapLevelScriptActor_B.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MenuMapLevelScriptActor_B.h"
 #include "Kismet/GameplayStatics.h"
 #include "Engine/World.h"
